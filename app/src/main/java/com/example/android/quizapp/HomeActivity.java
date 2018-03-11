f701c25fc8ef7469de71c78afbd7d0b97c82f891
liwona <liwona@users.noreply.github.com>
2018-03-11 20:16:41 +0100

    separate layout for each part

--- app/src/main/java/com/example/android/quizapp/HomeActivity.java
+++ app/src/main/java/com/example/android/quizapp/HomeActivity.java
@@ -1,10 +1,8 @@
 package com.example.android.quizapp;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.CheckBox;
